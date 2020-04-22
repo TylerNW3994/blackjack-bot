d0feb21dda8cc06e007b7fc5a89574a375719824
TylerNW3994 <TylerNW3994@gmail.com>
2020-04-22 16:01:38 -0400

    Merged Game.js and bot.js for somplicity

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const GAME = require("Game.js");
-var game;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -19,6 +17,22 @@ client.on('ready', () => {
 		Win 	: int
 */
 var players = {};
+var dealer = {};
+var newDeck = ["A", "A", "A", "A",
+			"1", "1", "1", "1",
+			"2", "2", "2", "2",
+			"3", "3", "3", "3",
+			"4", "4", "4", "4",
+			"5", "5", "5", "5",
+			"6", "6", "6", "6",
+			"7", "7", "7", "7",
+			"8", "8", "8", "8",
+			"9", "9", "9", "9",
+			"10", "10", "10", "10",
+			"J", "J", "J", "J",
+			"Q", "Q", "Q", "Q",
+			"K", "K", "K", "K"];
+var deck = newDeck;
 
 const CARDS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 const MAXPLAYERS = 7;
@@ -65,7 +79,7 @@ client.on('message', msg => {
 			else startingChips = 500;
 			msg.reply(" wants to start a game of Blackjack!  Who else wants to play?  Type !JoinBlackjack to play! We start with " + startingChips + " chips!");
 			gameOn = true;
-			game = new Game(startingChips);
+			//game = new Game(startingChips);
 			
 			msg.channel.send("Your dealer\s name is " + name);
 		}
@@ -99,32 +113,151 @@ client.on('message', msg => {
 	}
 	
 	if(command == "Hit" || command == "hit" || command == "!Hit"){
-		if(game.getCurrentTurn() == players[author].turn){
+		if(this..getCurrentTurn() == players[author].turn){
 			var currentPlayer = author;
 			//Add a card
 			var cardDealt = game.dealCard();
 			players[currentPlayer].cards[players[currentPlayer].cards.length] = cardDealt.indexOf(CARDS);
-			message.reply("you got a "  + game.deck[cardDealt]);
+			msg.reply("you got a "  + game.deck[cardDealt]);
 			game.addTotal(currentPlayer, cardDealt);
-			message.channel.send("Your new total is: " + currentPlayer.total);
-			message.channel.send("What would you like to do now?");
+			msg.channel.send("Your new total is: " + currentPlayer.total);
+			msg.channel.send("What would you like to do now?");
 		}
 	}
 	
 	if(command == "Stand" || command == "stand" || command == "!Stand"){
-		var currentPlayer = author;
-		game.setTurn(author.turn + 1);
-		var playedPlayers = [{}];
-		//Get all the players that played
-		for(var x in players){
-			if(x.played){
-				playedPlayers[x] = x;
+		if(this.getCurrentTurn() == players[author].turn){
+			var currentPlayer = author;
+			this.setTurn(author.turn + 1);
+			var playedPlayers = [{}];
+			//Get all the players that played
+			for(var x in players){
+				if(x.played){
+					playedPlayers[x] = x;
+				}
+			}
+			if(this.getTurn() ==  playedPlayers.length){
+				msg.channel.send("Okay, everyone went, let\'s see what cards I got!");
+				game.endRound(players, msg);
 			}
 		}
-		if(game.getTurn() ==  playedPlayers){
-			msg.channel.send("Okay, everyone went, let\'s see what cards I got!");
-			game.endRound(players);
+	}
+	
+	if(command == "Leave" || command == "leave" || command == "!Leave"){
+		if(players[author]){
+			//decrease the turn order of all the players after the leaving player
+			for(var i = author.indexOf(players); i < players.length; i++){
+				players[i].turn--;
+			}
+			msg.reply(" see you later!");
+			delete players[author];
+		} else {
+			msg
 		}
 	}
 });
-client.login(process.env.TOKEN);
+
+function newRound(players, msg){
+	msg.channel.send("Alright, time for another round!");
+	msg.channel.send(this.dealer.name + " is shuffling the cards...");
+	this.deck = newDeck;
+	shuffleDeck(this.deck);
+	//Remove all the cards from the players
+	for(var x in players){
+		x.cards = [];
+	}
+}
+
+function dealCard(){
+	var pulledCard = deck.pop();
+	return pulledCard;
+}
+
+function shuffleDeck(deck){
+	// for 1000 turns
+	// switch the values of two random cards
+	for (var i = 0; i < 1000; i++)
+	{
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
+
+		this.deck[location1] = deck[location2];
+		this.deck[location2] = tmp;
+	}
+}
+
+function endRound(players, msg){
+	while(getDealerTotal() < 17){
+		var cardDealt = this.dealCard();
+		this.dealer.cards[this.dealer.cards.length] = addTotal(this.dealer, cardDealt);
+		msg.channel.send("I got a "  + game.deck[cardDealt]);
+		msg.channel.send("My new total is: " + this.dealer.total);
+	}
+	
+	//Dealer bust
+	if(getDealerTotal() > 21){
+		message.channel.send("I bust!  You all win!");
+		for(var x in players){
+			x.win = true;
+		}
+	}
+	
+	//Dealer didn't bust but stayed past or at 17
+	else{
+		for(var x in players){
+			//Player won
+			if(x.total > getDealerTotal()){
+				x.chips += x.bet;
+				msg.channel.send(x + " won!  New chips amount: " + x.chips);
+			}
+			//Player tied
+			else if(x.total == getDealerTotal()){
+				msg.channel.send(x + " and I tied.  No change in chips!");
+			}
+			//Player lost
+			else if(x.total < getDealerTotal()){
+				x.chips -= x.bet;
+				msg.channel.send(x + " lost!  New chips amount: " + x.chips);
+			}
+			//ERROR
+			else {
+				msg.channel.send("Tyler, there\'s a mistake determining who won.");
+			}
+		}
+	}
+}
+
+//If a card is a face card, turn it into a 10.  Ace, turn into either  1 or 11.
+function addTotal(player, card){
+	if(card.indexOf(CARDS) > 9){
+		//Ace
+		if(card.indexOf(CARDS) == 13){
+			//Ace should be a 1
+			if(player.total >= 11)
+				player.total++;
+			//Ace should be an 11
+			else player.total += 11;
+		}
+		else player.total += 10;
+	}
+	else player.total += card.indexOf(CARDS) + 1;
+}
+
+function getDealerTotal(){
+	return this.dealer.total;
+}
+
+function getChips() {
+	return this.startingChips;
+}
+
+function getTurn() {
+	return this.currentTurn;
+}
+
+function setTurn(turn) {
+	this.currentTurn++;
+}
+
+client.login("NzAyNjA1MDYyNDIxMzQ4OTEy.XqCd-Q.BJJcPMdgreJ08TmmNZ91khftzKM");
